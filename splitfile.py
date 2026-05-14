09e34c5e7afa85f4e2c6538ff59cebd89b94355c
jackworthen <jworthen82@gmail.com>
2026-05-14 07:35:47 -0400

    fixed window size
    fixed window size on column filter window.

--- splitfile.py
+++ splitfile.py
@@ -25,7 +25,7 @@ class ColumnSelectionWindow:
         # Create window
         self.window = tk.Toplevel(parent)
         self.window.title("Column Filter")
-        self.window.geometry("500x370")
+        self.window.geometry("500x410")
         self.window.resizable(True, True)
         self.window.transient(parent)
         self.window.grab_set()
@@ -33,8 +33,8 @@ class ColumnSelectionWindow:
         # Center the window
         self.window.update_idletasks()
         x = (self.window.winfo_screenwidth() // 2) - (500 // 2)
-        y = (self.window.winfo_screenheight() // 2) - (450 // 2)
-        self.window.geometry(f"500x370+{x}+{y}")
+        y = (self.window.winfo_screenheight() // 2) - (410 // 2)
+        self.window.geometry(f"500x410+{x}+{y}")
         
         self.create_widgets()
         self.populate_lists()
